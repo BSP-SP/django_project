cdbcf24d56355b406e9dd6bbc4c46da7718edf9e
BSP-SP <57468978+BSP-SP@users.noreply.github.com>
2021-04-17 16:55:58 +0530

    init

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+posts=[
+    {
+        'author' :'Bineesh s p',
+        'title' : 'Blog post 1',
+        'content':'First post content',
+        'date_posted' : 'August 18 ,2018'
+    },
+    {
+        'author' :'Binoy s p',
+        'title' : 'Blog post 2',
+        'content':'Second post content',
+        'date_posted' : 'August 25 ,2018'
+    }
+]
+
+
 
 # Create your views here.
+def home(request):
+    context={
+        'posts':posts
+    }
+    return render(request, 'blog/home.html',context)
+
+def about(request):
+    return render(request,'blog/about.html',{ 'title':'About' })
+
